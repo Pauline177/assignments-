c5da10f603c91b062195916f97b0046843f2329b
Pauline Makoma <uwasepaldy@gmail.com>
2019-01-25 09:45:31 -0700

    commit message

---
+++ exercises/Grocery store js/app.js
@@ -0,0 +1,16 @@
+var shopper={ 
+    name: "john",
+    timesYouShopPerWeek: "3",
+    areYouVegetarian: "false",
+}
+    
+ function theClient(name,timesYouShopPerWeek){
+    console.log("Thank you "+ name + " for shoping "+ timesYouShopPerWeek+ " times a week with us.")
+
+}
+
+theClient(shopper.name, shopper.timesYouShopPerWeek)
+
+
+var groceryCart = [ "milk", "fruits", "bread", "juice"
+]
