aee0517e875c15148c554e2c828aecb7da0fe8e3
Pauline Makoma <uwasepaldy@gmail.com>
2019-01-28 11:23:05 -0700

    execirses

---
+++ exercises/ifstatement/app.js
@@ -0,0 +1,68 @@
+// var age="5"
+//     if (5>=3){
+//         console.log(true)
+//     }
+
+// var cat="3"
+//     if (cat="3"){
+//         console.log("is the length")
+//     }
+
+// var cat = "dog"
+//     if(cat == "rat"){
+//         console.log("cat is the same as the dog")
+//     } else
+//         console.log("cat is not the same as the dog")
+    
+// var person = {
+//     name: "boo",
+//     age: 20 }
+
+//   if(person.name.startsWith("b") && (person.age >= 18)){
+//       console.log(person.name+" is allowed to go to the movie")
+//   } else {
+//       console.log(person.name+" is not allowed to go to the movie")
+//   }
+
+// var num ="1"
+// if(num===1){
+//     console.log("strict")
+// } else (num==1) 
+//     console.log("loose")
+
+// var num1 = "1"
+//     num2 ="2"
+
+// if (((num1 <= num2) && (num2 == "4")) || (((3*4) > "10") && ((5+10)> "10"))){
+//     console.log("yes")
+// } else {
+//     console.log(false)
+// }
+
+// (((3*4) > "10") && ((5+10)> "10"))
+
+//  var dog = "dudley"               //for gold1
+//  var answer = true               //for gold2
+
+// if (typeof(dog) === 'string') {
+//     console.log("string")
+// }
+// if (typeof(answer) === "boolean" ) {
+//     console.log("boolean")
+// }
+
+// var num = ' '
+// if (num) {
+//     console.log("it is defined")
+// } else {
+//     console.log("it's undefined")
+// }
+
+var myNum = 10
+for(i= 0; i<=10; i+=2) {
+    console.log(i)
+    
+}
+
+ 
+ 
---
+++ exercises/loopolympics/app.js
@@ -0,0 +1,47 @@
+
+// for(i=0; i<10; i++){
+//     console.log(i)
+// }
+
+// for(i=9; i>=0; i--){
+//     console.log(i)
+// }
+
+// var fruit = ["banana", "orange", "apple", "kiwi"]
+// fruit.push("beets","pear","peach")   
+// for(var i=0; i <fruit.length; i++)  {
+// console.log(fruit[i])
+// }
+
+
+// for( i=0; i<100; i+=2){
+//     console.log(i)
+// }
+
+// var peopleArray = [
+//     {
+//       name: "Harrison Ford",
+//       occupation: "Actor"
+//     },
+//     {
+//       name: "Justin Bieber",
+//       occupation: "Singer"
+//     },
+//     {
+//       name: "Vladimir Putin",
+//       occupation: "Politician"
+//     },
+//     {
+//       name: "Oprah",
+//       occupation: "Entertainer"
+//     }
+//   ]
+//  for (var i = 0; i < peopleArray.length; i++) {
+
+//     console.log(peopleArray[i].name, peopleArray[i].occupation)
+// }
+
+var arr= ["banana", "orange", "pineapple", "kiwi"]
+for( var i = 0; i < arr.length; i++) {
+console.log(arr[i])
+}
