65e8c5defe36eaaa70fd20d278bfcd2af9207afb
Pauline Makoma <uwasepaldy@gmail.com>
2019-03-13 10:06:06 -0600

    wednesday

--- exercises/bounty-hunter/Routes/route.js
+++ exercises/bounty-hunter/Routes/route.js
@@ -2,7 +2,8 @@ const express = require('express')
 const userRouter = express.Router()
 const uuid = require('uuid/v4')
 
-const bountyHunters = [
+// it needs to be let otherwise the variable will not be over-written
+let bountyHunters = [
     {
         first: "Shade",
         last: "Charm",
@@ -43,14 +44,28 @@ userRouter.route("/")
     .get((req, res) => {
         res.send(bountyHunters)
     })
-    .post()
+    .post((req, res) => {
+        const addedBounty = req.body
+        addedBounty._id = uuid()
+        bountyHunters.push(addedBounty)
+        res.send(addedBounty)
+    })
 
 
 userRouter.route("/:_id")
-    .get()
-    .delete()
-    .put()
-
-
+    .get((req, res) => {
+        const foundBounty = bountyHunters.find(hunter => hunter._id === req.params._id)
+        res.send(foundBounty)
+    })
+    .delete((req, res) => {
+        const removedBounty = bountyHunters.filter(hunter => hunter._id !== req.params._id)
+        bountyHunters = removedBounty
+        res.send(bountyHunters)
+    })
+    .put((req,res) => {
+        const foundBounty = bountyHunters.find(hunter => hunter._id === req.params._id)
+        Object.assign(foundBounty, req.body)
+        res.send(foundBounty)
+    })
 
 module.exports = userRouter
