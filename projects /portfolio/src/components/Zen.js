e2d4fa620e8ba1622e98c594f33783b7cafdc6cf
Pauline Makoma <uwasepaldy@gmail.com>
2019-04-11 16:36:50 -0600

    thursday

--- projects /portfolio/src/components/Zen.js
+++ projects /portfolio/src/components/Zen.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 const Zen = () => {
 
-    return(
+    return (
         <div className="window">
-            zen zen gardern 
+            zen zen gardern
             <div>
                 <img src="https://mail.google.com/mail/u/0?ui=2&ik=a513a9f0da&attid=0.1&permmsgid=msg-a:r7955921847045399977&th=16a0e1657e684dd5&view=att&disp=safe&realattid=f_jud3h4q90" />
+            </div>
+            <div>
+                <p>
+                    ZenGardern is a front-end site that i built using html and css
+                </p>
             </div>
         </div>
     )
 }
 
-export default  Zen
+export default Zen
